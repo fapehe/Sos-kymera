1082e053659a4a58d06a08ee3b6ee056c833551f
fapehe <fabianperaltahernandez@gmail.com>
2017-05-27 23:25:24 -0500

    correcion acceso a base de datos

--- app/src/main/java/com/example/fapehe/sos_kymera/Registrador.java
+++ app/src/main/java/com/example/fapehe/sos_kymera/Registrador.java
@@ -8,6 +8,7 @@ import android.provider.BaseColumns;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -97,14 +98,13 @@ public class Registrador extends SQLiteOpenHelper implements DownloadCallback{
             connection.setDoInput(true);
             // Open communications link (network traffic occurs here).
             connection.connect();
-            publishProgress(DownloadCallback.Progress.CONNECT_SUCCESS);
             int responseCode = connection.getResponseCode();
             if (responseCode != HttpsURLConnection.HTTP_OK) {
                 throw new IOException("HTTP error code: " + responseCode);
             }
             // Retrieve the response body as an InputStream.
             stream = connection.getInputStream();
-            publishProgress(DownloadCallback.Progress.GET_INPUT_STREAM_SUCCESS, 0);
+
             if (stream != null) {
                 // Converts Stream to String with max length of 500.
                 result = readStream(stream, 500);
@@ -120,6 +120,32 @@ public class Registrador extends SQLiteOpenHelper implements DownloadCallback{
         }
         return result;
     }
+
+    private String readStream(InputStream stream, int maxLength) throws IOException {
+        String result = null;
+        // Read InputStream using the UTF-8 charset.
+        InputStreamReader reader = new InputStreamReader(stream, "UTF-8");
+        // Create temporary buffer to hold Stream data with specified max length.
+        char[] buffer = new char[maxLength];
+        // Populate temporary buffer with Stream data.
+        int numChars = 0;
+        int readSize = 0;
+        while (numChars < maxLength && readSize != -1) {
+            numChars += readSize;
+            int pct = (100 * numChars) / maxLength;
+
+            readSize = reader.read(buffer, numChars, buffer.length - numChars);
+        }
+        if (numChars != -1) {
+            // The stream was not empty.
+            // Create String that is actual length of response body if actual length was less than
+            // max length.
+            numChars = Math.min(numChars, maxLength);
+            result = new String(buffer, 0, numChars);
+        }
+        return result;
+    }
+
 
 
 }
